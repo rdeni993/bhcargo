e65e596360247a002bfb72d5e28186331111d6c0
rdeni993 <denis.ristic3@gmail.com>
2021-02-08 21:24:17 +0100

    Add CDN

--- app/Views/meta/header.php
+++ app/Views/meta/header.php
@@ -14,14 +14,14 @@
         <link rel="shortcut icon" href="<?php echo base_url('favicon.ico'); ?>">
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet"> 
-        <link rel="stylesheet" href="<?php echo base_url('public/assets/css/bootstrap.min.css'); ?>" type="text/css" />
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" type="text/css" />
         <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
         <link rel="stylesheet" href="<?php echo base_url('public/assets/css/style.min.css'); ?>" type="text/css" />
         <link rel="stylesheet" href="<?php echo base_url('public/assets/css/fa/css/font-awesome.min.css'); ?>" type="text/css" />
 
         <!-- Load JS -->
-        <script type="text/javascript" src="<?php echo base_url('public/assets/js/jquery.min.js') ?>"></script>
-        <script type="text/javascript" src="<?php echo base_url('public/assets/js/bootstrap.bundle.min.js') ?>"></script>
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+        <script type="text/javascript" src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
         <script type="text/javascript" src="<?php echo base_url('public/assets/js/script.min.js') ?>"></script>
 
         <!-- Google Analytics --><!-- Global site tag (gtag.js) - Google Analytics -->
